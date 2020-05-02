5df2f26dbf5575f3eb599045c273fbaed1c2b11d
abhishek7060 <abhishek7060@gmail.com>
2020-05-02 17:31:32 +0530

    fixes

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,69 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent implements OnInit {
+  title = 'myProfile';
+  public myProfession = [" a web designer.", " a Full Stack Developer.", " a nature lover.", " an amateur gamer."];
+  loopNum: number;
+  el: any;
+  toRotate: any;
+  period: number;
+  txt: string;
+  isDeleting: boolean;
+
+  constructor() { }
+
+  ngOnInit() {
+    var elements = document.getElementsByClassName('typewrite');
+    for (var i = 0; i < elements.length; i++) {
+      if (this.myProfession) {
+        this.TxtType(elements[i], this.myProfession, 2000);
+      }
+    }
+  }
+
+  TxtType(el, toRotate, period) {
+    this.toRotate = toRotate;
+    this.el = el;
+    this.loopNum = 0;
+    this.period = parseInt(period, 10) || 2000;
+    this.txt = '';
+    this.tick();
+    this.isDeleting = false;
+  };
+
+  tick() {
+    var i = this.loopNum % this.toRotate.length;
+    var fullTxt = this.toRotate[i];
+
+    if (this.isDeleting) {
+      this.txt = fullTxt.substring(0, this.txt.length - 1);
+    } else {
+      this.txt = fullTxt.substring(0, this.txt.length + 1);
+    }
+
+    this.el.innerHTML = 'I am <span class="wrap" style="font-weight: bold;">' + this.txt + '</span>';
+
+    var that = this;
+    var delta = 200 - Math.random() * 100;
+
+    if (this.isDeleting) { delta /= 2; }
+
+    if (!this.isDeleting && this.txt === fullTxt) {
+      delta = this.period;
+      this.isDeleting = true;
+    } else if (this.isDeleting && this.txt === '') {
+      this.isDeleting = false;
+      this.loopNum++;
+      delta = 500;
+    }
+
+    setTimeout(function () {
+      that.tick();
+    }, delta);
+  }
+}
